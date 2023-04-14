773526e1bf6213a67869c644020fdec854b0caae
EunhaKyeong <kyeongeh424@gmail.com>
2023-04-14 17:47:34 +0900

    Feat: 데이터가 잘 오는지 확인하기 위한 로그 작성

--- app/src/main/java/com/example/covid_vaccine/viewmodel/SplashViewModel.kt
+++ app/src/main/java/com/example/covid_vaccine/viewmodel/SplashViewModel.kt
@@ -6,6 +6,8 @@ import androidx.lifecycle.viewModelScope
 import com.example.covid_vaccine.repository.CenterRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
 import javax.inject.Inject
 
 @HiltViewModel
@@ -13,7 +15,18 @@ class SplashViewModel @Inject constructor(private val centerRepository: CenterRe
     fun getCenters(page: Int) {
         viewModelScope.launch {
             centerRepository.getCenters(page).collect {
-                Log.d("SplashViewModel", "getCenters: $it")
+                it.onSuccess {centers ->
+                    Log.i("SplashViewModel", "getCenters: $centers")
+                }
+
+                it.onFailure {e ->
+                    when (e) {
+                        is IOException -> Log.e("SplashViewModel", "[IOException] ${e.message}")
+                        is HttpException -> Log.e("SplashViewModel", "[HttpException] code: ${e.response()?.code()}, body: ${e.response()?.body()}")
+                        is Exception -> Log.e("SplashViewModel", "[Exception] ${e.message}")
+                    }
+                }
+
             }
         }
     }
