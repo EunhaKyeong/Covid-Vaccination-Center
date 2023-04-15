d0bbfb52a7444c00aa3d6bfcdf1672830a103209
EunhaKyeong <kyeongeh424@gmail.com>
2023-04-16 02:55:47 +0900

    Style: Remove Center.kt

--- app/src/main/java/com/example/covid_vaccine/domain/Center.kt
+++
@@ -1,9 +0,0 @@
-package com.example.covid_vaccine.domain
-
-data class Center(
-    val address: String,
-    val centerName: String,
-    val facilityName: String,
-    val phoneNumber: String,
-    val updatedAt: String
-)
